f52e2adc9814c290b8ac8498871071c03e076305
Egor Smolenskii <smolenlr@gmail.com>
2023-01-02 13:52:36 +0700

    first commit

---
+++ advertisements/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from .models import Advertisement
+
+@admin.register(Advertisement)
+class StockAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'description', 'created_at', 'updated_at', 'status', 'creator')
